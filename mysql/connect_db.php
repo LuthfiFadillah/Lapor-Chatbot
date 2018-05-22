1cff4248405235c7409a04dca5a187e7b91ab028
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2018-05-22 16:52:14 +0700

    edit for postgres connection

--- mysql/connect_db.php
+++ mysql/connect_db.php
@@ -8,14 +8,20 @@
 
 function connect_db(){
     try{
-        $host = 'localhost';
-        $database = 'lapor-chatbot-3';
-        $username = 'root';
-        $password = '';
+        $dotenv = new Dotenv\Dotenv(realpath($_SERVER["DOCUMENT_ROOT"]).'/Lapor-Chatbot-dev/' );
+        $dotenv->load();
 
+
+        $connection = getenv('DB_CONNECTION','pgsql');
+        $host = getenv('DB_HOST','localhost');
+        $database = getenv('DB_DATABASE','lapor-chatbot');
+        $username = getenv('DB_USERNAME','postgres');
+        $password = getenv('DB_PASSWORD','');
+
+        error_log($connection, 0);
         global $con_db;
 
-        $con_db = new PDO("mysql:host=".$host.";dbname=".$database,$username,$password);
+        $con_db = new PDO($connection.":host=".$host.";dbname=".$database.";user=".$username.";password=".$password);
 
         if(!isset($con_db)){
             error_log("Connection to database failure!",0);
